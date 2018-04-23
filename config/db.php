b6ff604251193060076ff5b1c63bd7c718286de0
celsoft <maksim_blg@mail.ru>
2018-04-23 20:39:37 +0900

    Updated

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=system_smu28_su',
     'username' => 'root',
-    'password' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
